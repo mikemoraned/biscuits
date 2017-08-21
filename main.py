3e25712737dd467bf576a4323b8b6b2c6f7e0e05
Mike Moran <mike@houseofmoran.com>
2017-08-21 22:49:15 +0100

    first version which finds labelled areas
    based largely on
    https://stackoverflow.com/questions/35854197/how-to-use-opencvs-connected-components-with-stats-in-python

---
+++ main.py
@@ -0,0 +1,23 @@
+# Import the cv2 library
+import cv2
+# Read the image you want connected components of
+src = cv2.imread('edinburgh.png')
+# Convert to grayscale
+src_grey = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+# Threshold it so it becomes binary
+ret, thresh = cv2.threshold(src_grey,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+# You need to choose 4 or 8 for connectivity type
+connectivity = 4
+# Perform the operation
+output = cv2.connectedComponentsWithStats(thresh, connectivity, cv2.CV_32S)
+# Get the results
+# The first cell is the number of labels
+num_labels = output[0]
+# The second cell is the label matrix
+labels = output[1]
+# The third cell is the stat matrix
+stats = output[2]
+# The fourth cell is the centroid matrix
+centroids = output[3]
+
+cv2.imwrite('edinburgh.labels.png', labels)
