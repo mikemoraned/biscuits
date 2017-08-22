8a1513bd22591196849cb7156facacd7841f56bd
Mike Moran <mike@houseofmoran.com>
2017-08-23 00:24:22 +0100

    add animation based on d3-ease

--- vis/src/App.js
+++ vis/src/App.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'isomorphic-fetch';
+import { easeCubicInOut } from "d3-ease";
 
 class ConnectedComponents extends Component {
 
     constructor() {
         super();
 
-        this.state = { components: [] };
+        this.state = {
+            components: [],
+            t: 0.0,
+            transitionDuration: 8000,
+            ease: easeCubicInOut
+        };
     }
 
     componentDidMount() {
+
+        const setT = () => {
+            const currentDate = new Date();
+            const t = (currentDate.getTime() % this.state.transitionDuration) / this.state.transitionDuration;
+            this.setState({ t });
+            requestAnimationFrame(setT);
+        }
+        setT();
+
         return fetch("edinburgh.labels.json")
             .then((response) => response.json())
             .then((json) => {
@@ -20,14 +35,18 @@ class ConnectedComponents extends Component {
             });
     }
 
+    t() {
+        return this.state.ease(this.state.t);
+    }
+
     render() {
         return (
             <div className="components">
                 {
                     this.state.components.map((component) => {
                         const style = {
-                            left: component.x,
-                            top: component.y,
+                            left: this.t() * component.x,
+                            top: this.t() * component.y,
                             height: component.height,
                             width: component.width,
                             backgroundPositionX: -component.x,
