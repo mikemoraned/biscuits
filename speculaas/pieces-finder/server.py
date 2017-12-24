7f87d5f1edcab6c7fecf0326b74cb5e5af35481b
Mike Moran <mike@houseofmoran.com>
2017-12-24 01:43:53 +0100

    add minimal server

---
+++ speculaas/pieces-finder/server.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_graphql import GraphQLView
+import graphene
+
+
+class Query(graphene.ObjectType):
+    hello = graphene.String(name=graphene.String(default_value="stranger"))
+
+    def resolve_hello(self, info, name):
+        return 'Hello ' + name
+
+
+schema = graphene.Schema(query=Query)
+
+app = Flask(__name__)
+app.debug = True
+
+app.add_url_rule(
+    '/graphql',
+    view_func=GraphQLView.as_view(
+        'graphql',
+        schema=schema,
+        graphiql=True
+    )
+)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
