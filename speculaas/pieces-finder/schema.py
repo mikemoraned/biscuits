7a09bc7c58e1f354b0c690eb653132368e7cdac1
Mike Moran <mike@houseofmoran.com>
2017-12-25 20:36:00 +0100

    make it return a (dummy) bitmap image

--- speculaas/pieces-finder/schema.py
+++ speculaas/pieces-finder/schema.py
@@ -1,18 +1,29 @@
 import graphene
 
+DUMMY_PNG = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfF" \
+            "cSJAAAADUlEQVR42mNkYPhfDwAChwGA60e6kgAAAABJRU5ErkJggg=="
 
-class FeatureCollection(graphene.ObjectType):
-    features = graphene.List(graphene.String)
 
-    def resolve_features(self, info):
-        return []
+class BitmapImage(graphene.ObjectType):
+    data = graphene.Field(graphene.String)
+    x = graphene.Field(graphene.Int)
+    y = graphene.Field(graphene.Int)
+    width = graphene.Field(graphene.Int)
+    height = graphene.Field(graphene.Int)
+
+
+class Piece(graphene.ObjectType):
+    bitmapImage = graphene.Field(BitmapImage)
+
+    def resolve_bitmapImage(self, info):
+        return BitmapImage(data=DUMMY_PNG, x=0, y=0, width=100, height=200)
 
 
 class Query(graphene.ObjectType):
-    pieces = graphene.Field(FeatureCollection)
+    pieces = graphene.Field(Piece)
 
     def resolve_pieces(self, info):
-        return FeatureCollection()
+        return [Piece()]
 
 
 schema = graphene.Schema(query=Query)
--- speculaas/pieces-finder/schema_test.py
+++ speculaas/pieces-finder/schema_test.py
@@ -1,20 +1,45 @@
 import unittest
+from collections import OrderedDict
+
 from graphene.test import Client
 from schema import schema
 
 
 class TestSchema(unittest.TestCase):
+    def __init__(self, method_name):
+        super().__init__(method_name)
+        self.maxDiff = None
 
     def test_pieces(self):
         client = Client(schema)
-        executed = client.execute('''{ pieces { features } }''')
-        self.assertEqual(executed, {
-            'data': {
-                'pieces': {
-                    'features': []
+        executed = client.execute('''
+        { 
+            pieces { 
+                bitmapImage {
+                    data,
+                    x,
+                    y,
+                    width,
+                    height
                 }
-            }
-        })
+            } 
+        }''')
+        expected = OrderedDict(
+            [('data', OrderedDict(
+                [('pieces', OrderedDict(
+                    [('bitmapImage', OrderedDict(
+                        [('data', "data:image/png;base64,iVBORw0KGgoAAAANSUh"
+                                  "EUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk"
+                                  "YPhfDwAChwGA60e6kgAAAABJRU5ErkJggg=="),
+                         ('x', 0),
+                         ('y', 0),
+                         ('width', 100),
+                         ('height', 200)]
+                    ))]
+                ))]
+            ))]
+        )
+        self.assertEqual(executed, expected)
 
 
 if __name__ == '__main__':
