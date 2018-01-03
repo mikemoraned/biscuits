eacb42f638fbf5d2bbf8fd453000566bba34eff8
Mike Moran <mike@houseofmoran.com>
2018-01-03 23:57:48 +0100

    round down to max_width/max_height of binned rects

--- speculaas/pieces-finder/repack.py
+++ speculaas/pieces-finder/repack.py
@@ -5,13 +5,16 @@ import numpy as np
 
 from precomputed_lookup_splitter import PreComputedLookupSplitter
 
-precomputed_dir = sys.argv[1]
-place_ids = sys.argv[2].split(',')
-splitter = PreComputedLookupSplitter.from_dir(precomputed_dir)
-print("Loaded {} ids from {}".format(len(splitter.place_ids),
-                                     precomputed_dir))
 
-for place_id in place_ids:
+def convert_binned_to_cv2_rectangles(rect_list):
+    for rect in rect_list:
+        (bin_count, x, y, width, height, rid) = rect
+        top_left = (x, y)
+        bottom_right = (x + width, y + height)
+        yield (top_left, bottom_right)
+
+
+def repack(splitter, place_id):
     print("Doing {}".format(place_id))
     place = splitter.split(place_id)
     packer = newPacker()
@@ -23,14 +26,34 @@ for place_id in place_ids:
     packer.add_bin(width=10000, height=10000)
 
     packer.pack()
+    cv2_rects = list(convert_binned_to_cv2_rectangles(packer.rect_list()))
+    max_width = -1
+    max_height = -1
+    for cv2_rect in cv2_rects:
+        (top_left, bottom_right) = cv2_rect
+        (x, y) = bottom_right
+        max_width = max(max_width, x)
+        max_height = max(max_height, y)
 
-    img = np.zeros((10000, 10000, 3), np.uint8)
+    print("{}: {} rects, width={}, height={}".format(place_id,
+                                                     len(cv2_rects),
+                                                     max_width,
+                                                     max_height))
+
+    img = np.zeros((max_height, max_width, 3), np.uint8)
     color = (0, 255, 0)
-    for rect in packer.rect_list():
-        print(rect)
-        (bin_count, x, y, width, height, rid) = rect
-        top_left = (x, y)
-        bottom_right = (x + width, y + height)
+    for cv2_rect in cv2_rects:
+        (top_left, bottom_right) = cv2_rect
         cv2.rectangle(img, top_left, bottom_right, color, 3)
 
     cv2.imwrite('{}_repacked.png'.format(place_id), img)
+
+
+if __name__ == '__main__':
+    precomputed_dir = sys.argv[1]
+    place_ids = sys.argv[2].split(',')
+    splitter = PreComputedLookupSplitter.from_dir(precomputed_dir)
+    print("Loaded {} ids from {}".format(len(splitter.place_ids),
+                                         precomputed_dir))
+    for place_id in place_ids:
+        repack(splitter, place_id)
