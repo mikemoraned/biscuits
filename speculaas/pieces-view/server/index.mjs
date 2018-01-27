88bb6f3f8b8e6a8c8e161222b9ee72051171a922
Mike Moran <mike@houseofmoran.com>
2018-01-27 18:12:44 +0000

    pieces-view: refactor graphql healthcheck into a graphqlContactable Promise

--- speculaas/pieces-view/server/index.mjs
+++ speculaas/pieces-view/server/index.mjs
@@ -7,32 +7,41 @@ const app = express();
 const port = process.argv[2];
 const graphqlServerUrl = process.argv[3];
 
+const graphqlContactable = (serverURL) => new Promise((resolve, reject) => {
+  const headers = new Headers();
+  headers.append("Accept", "text/html")
+  const healthcheckURL = serverURL + "graphql";
+  fetch(healthcheckURL, { headers }).then((response) => {
+    if (response.ok) {
+      const message = `graphql: OK, URL: ${healthcheckURL}`;
+      resolve(message);
+    }
+    else {
+      const message = `graphql: non OK status, ${response.status}, URL: ${healthcheckURL}`;
+      reject(message);
+    }
+  }).catch((error) => {
+    const message = `graphql: Error, URL: ${healthcheckURL}`;
+    reject(message);
+  });
+});
+
 app.use('/graphql', proxy({target: graphqlServerUrl, changeOrigin: true}));
 app.get('/healthcheck/alive', (req, res) => res.send("Alive"));
 app.get('/healthcheck/ready', (req, res) => {
-    const headers = new Headers();
-    headers.append("Accept", "text/html")
-    const healthcheckURL = graphqlServerUrl + "graphql";
-    fetch(healthcheckURL, { headers }).then((response) => {
-      if (response.ok) {
-        const message = `graphql: OK, URL: ${healthcheckURL}`;
-        console.log(message);
-        res.send("Ready");
-      }
-      else {
-        const message = `graphql: non OK status, ${response.status}, URL: ${healthcheckURL}`;
-        console.error(message);
-        res.status(500).send(message);
-      }
-    }).catch((error) => {
-      const message = `graphql: Error, URL: ${healthcheckURL}`;
-      console.error(error);
+  graphqlContactable(graphqlServerUrl)
+    .then((message) => {
+      console.log(message);
+      res.send("Ready");
+    })
+    .catch((message) => {
+      console.error(message);
       res.status(500).send(message);
     })
 });
 app.use('/', express.static('../build'));
 
 app.listen(port, () => {
-    console.log('listening on: ', port);
-    console.log('graphql server: ', graphqlServerUrl);
+  console.log('listening on: ', port);
+  console.log('graphql server: ', graphqlServerUrl);
 });
