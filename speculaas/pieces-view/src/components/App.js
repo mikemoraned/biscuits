12142b092a79086a04e0a08b2a5d9ff759bb7561
Mike Moran <mike@houseofmoran.com>
2018-03-03 20:54:16 +0000

    add usable TransitionControl at bottom/top for portrait/landscape

--- speculaas/pieces-view/src/components/App.js
+++ speculaas/pieces-view/src/components/App.js
@@ -2,6 +2,32 @@ import React, { Component } from 'react';
 import '../styles/App.css';
 import PlaceList from "./PlaceList";
 
+class TransitionControl extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onValueChange = this.onValueChange.bind(this);
+
+    console.log(`constructor, : ${this.props.transitionProportion}`);
+  }
+
+  onValueChange(event) {
+    const value = event.target.value;
+    console.log(`onChange, value: ${value}`);
+    this.props.onChange(value / 100.0);
+  }
+
+  render() {
+    console.log(`render, t: ${this.props.transitionProportion}`);
+    return <div className={this.props.className}>
+      <input style={{width: "100%"}}
+             type="range" min="0" max="100" step="1"
+             value={this.props.transitionProportion * 100}
+             onChange={this.onValueChange}></input>
+    </div>
+  }
+}
+
 class App extends Component {
 
   constructor(props) {
@@ -14,19 +40,18 @@ class App extends Component {
     this.handleTransitionProportionChange = this.handleTransitionProportionChange.bind(this);
   }
 
-  handleTransitionProportionChange(event) {
-    const value = event.target.value;
+  handleTransitionProportionChange(t) {
     this.setState({
-      t: (value / 100.0)
+      t
     });
   }
 
   render() {
     return (
       <div className="App">
-        <input style={{ width: "90%", position: "sticky", top: "10px" }}
-               type="range" min="0" max="100" step="1"
-               value={this.state.t * 100} onChange={this.handleTransitionProportionChange}></input>
+        <TransitionControl className="TransitionControl_top"
+                           transitionProportion={this.state.t}
+                           onChange={this.handleTransitionProportionChange}/>
         <PlaceList transitionProportion={this.state.t}
                    placeIds={['edinburgh', 'jerusalem', 'au', 'newyork', 'budapest']}/>
         {/*<PlaceList transitionProportion={this.state.t}*/}
@@ -36,6 +61,9 @@ class App extends Component {
         {/*<PlaceList placeIds={['edinburgh']}/>*/}
         {/*<PlaceList placeIds={['edinburgh','budapest']}/>*/}
         {/*<PlaceList placeIds={['jerusalem']}/>*/}
+        <TransitionControl className="TransitionControl_bottom"
+                           transitionProportion={this.state.t}
+                           onChange={this.handleTransitionProportionChange}/>
       </div>
     );
   }
