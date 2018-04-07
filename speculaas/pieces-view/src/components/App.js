b61382c1d540b0c65da87a75cec3e34239b36415
Mike Moran <lazysurefix@gmail.com>
2018-04-08 00:46:32 +0100

    pv: extract TransitionControl

--- speculaas/pieces-view/src/components/App.js
+++ speculaas/pieces-view/src/components/App.js
@@ -1,32 +1,7 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
 import PlaceList from "./PlaceList";
-
-class TransitionControl extends Component {
-  constructor(props) {
-    super(props);
-
-    this.onValueChange = this.onValueChange.bind(this);
-
-    console.log(`constructor, : ${this.props.transitionProportion}`);
-  }
-
-  onValueChange(event) {
-    const value = event.target.value;
-    console.log(`onChange, value: ${value}`);
-    this.props.onChange(value / 100.0);
-  }
-
-  render() {
-    console.log(`render, t: ${this.props.transitionProportion}`);
-    return <div className={this.props.className}>
-      <input style={{width: "100%"}}
-             type="range" min="0" max="100" step="1"
-             value={this.props.transitionProportion * 100}
-             onChange={this.onValueChange}></input>
-    </div>
-  }
-}
+import {TransitionControl} from "./TransitionControl";
 
 class App extends Component {
 
---
+++ speculaas/pieces-view/src/components/TransitionControl.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+export class TransitionControl extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onValueChange = this.onValueChange.bind(this);
+  }
+
+  onValueChange(event) {
+    const value = event.target.value;
+    this.props.onChange(value / 100.0);
+  }
+
+  render() {
+    return <div className={this.props.className}>
+      <input style={{width: "100%"}}
+             type="range" min="0" max="100" step="1"
+             value={this.props.transitionProportion * 100}
+             onChange={this.onValueChange}></input>
+    </div>
+  }
+}
+
