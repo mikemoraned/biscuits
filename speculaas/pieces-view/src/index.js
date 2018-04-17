5bf345e0698d015b2899af5ad4691a08452e4dfa
Mike Moran <lazysurefix@gmail.com>
2018-04-17 22:46:54 +0100

    migrate to apollo-boost

--- speculaas/pieces-view/src/index.js
+++ speculaas/pieces-view/src/index.js
@@ -2,16 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
+import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
-import { ApolloClient } from 'apollo-client';
-import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
 
-const httpLink = new HttpLink({ uri: '/graphql' });
 const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache(),
-  connectToDevTools: true
+  uri: '/graphql'
 });
 
 ReactDOM.render(
