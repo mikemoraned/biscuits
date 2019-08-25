c99e93e0795aaa5c81ad78ecab7cdb9a11084aa8
Mike Moran <mike@houseofmoran.com>
2019-08-25 13:57:13 +0200

    add current viewport center as a geojson point

--- garibaldi/mapbox-frontend/src/App.js
+++ garibaldi/mapbox-frontend/src/App.js
@@ -1,17 +1,41 @@
-import React, { useState, useRef, useLayoutEffect, useCallback } from "react";
+import React, {
+  useState,
+  useRef,
+  useLayoutEffect,
+  useEffect,
+  useCallback
+} from "react";
 import "./App.css";
 import ReactMapGL from "react-map-gl";
 import dotenv from "dotenv";
 
 dotenv.config();
 
+function geoJSONPoint({ latitude, longitude }) {
+  return {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [longitude, latitude]
+    },
+    properties: {
+      name: "Hello World"
+    }
+  };
+}
+
 function Map() {
   const [viewport, setViewport] = useState({
-    width: 400,
-    height: 400,
+    width: "100vh",
+    height: "100vh",
     latitude: 37.7577,
     longitude: -122.4376,
     zoom: 8
+  });
+
+  const [center, setCenter] = useState({
+    latitude: viewport.latitude,
+    longitude: viewport.longitude
   });
 
   const containerRef = useRef();
@@ -28,12 +52,49 @@ function Map() {
     resizeObserver.observe(containerRef.current);
   }, [containerRef, resizeHandler]);
 
+  const mapRef = useRef();
+
+  function onLoad() {
+    const map = mapRef.current.getMap();
+    console.log("defining layer");
+    map.addSource("point", { type: "geojson", data: null });
+    map.addLayer({
+      id: "point",
+      type: "circle",
+      source: "point"
+    });
+    setCenter({
+      latitude: viewport.latitude,
+      longitude: viewport.longitude
+    });
+  }
+
+  function viewportUpdated(viewport) {
+    setViewport(viewport);
+    setCenter({
+      latitude: viewport.latitude,
+      longitude: viewport.longitude
+    });
+  }
+
+  useEffect(() => {
+    const map = mapRef.current.getMap();
+    if (map) {
+      const source = map.getSource("point");
+      if (source) {
+        source.setData(geoJSONPoint(center));
+      }
+    }
+  }, [mapRef, center]);
+
   return (
     <div ref={containerRef} className="fullscreen">
       <ReactMapGL
+        ref={mapRef}
         {...viewport}
         {...containerDimensions}
-        onViewportChange={viewport => setViewport(viewport)}
+        onViewportChange={viewportUpdated}
+        onLoad={onLoad}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
       />
     </div>
