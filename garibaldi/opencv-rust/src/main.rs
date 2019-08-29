adb294c71d5ac6bbe8e179ad5e51e63a9e9b4f73
Mike Moran <mike@houseofmoran.com>
2019-08-30 00:11:22 +0100

    compiling but getting error
    `Err(Error { code: -205, message: "Mat type is: CV_32SC1, but requested type is: CV_8UC1" })`

--- garibaldi/opencv-rust/src/main.rs
+++ garibaldi/opencv-rust/src/main.rs
@@ -10,9 +10,9 @@ fn detect_components() -> Result<()> {
     let components_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("components.png");
 
     let src = imgcodecs::imread(binary_path.to_str().unwrap(), imgcodecs::IMREAD_GRAYSCALE)?;
-    let mut labels = unsafe { Mat::new_size(src.size()?, core::CV_32S)? };
+    let mut label_image = unsafe { Mat::new_size(src.size()?, core::CV_32S)? };
 
-    let num_labels = imgproc::connected_components(&src, &mut labels, 8, core::CV_32S)?;
+    let num_labels = imgproc::connected_components(&src, &mut label_image, 8, core::CV_32S)?;
     println!("num labels: {}", num_labels);
 
     let mut color_map: Vec<Vec3b> = vec![Vec3b::default(); num_labels as usize];
@@ -28,9 +28,15 @@ fn detect_components() -> Result<()> {
     println!("color map: {:?}", color_map);
 
     let mut dest = unsafe { Mat::new_size(src.size()?, core::CV_8UC3)? };
+    for row in 0..dest.rows()? {
+        for col in 0..dest.cols()? {
+            let label = label_image.at_2d::<u8>(row, col)?;
+            *dest.at_2d_mut::<Vec3b>(row, col)? = color_map[*label as usize];
+        }
+    }
 
-    let params = opencv::types::VectorOfint::new();
-    imgcodecs::imwrite(components_path.to_str().unwrap(), &labels, &params)?;
+    // let params = opencv::types::VectorOfint::new();
+    // imgcodecs::imwrite(components_path.to_str().unwrap(), &labels, &params)?;
 
     Ok(())
 }
