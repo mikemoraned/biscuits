5d8d29cc8aa3d9d4e031d6f9d8c2e456a08df646
Mike Moran <mike@houseofmoran.com>
2019-08-31 23:16:16 +0100

    fixed; now applies a threshold before finding components

--- garibaldi/opencv-rust/src/main.rs
+++ garibaldi/opencv-rust/src/main.rs
@@ -1,18 +1,18 @@
 use rand::prelude::*;
 use std::path::PathBuf;
 
-use opencv::{
-    core, core::Vec3b, imgcodecs, imgproc, objdetect, prelude::*, types::VectorOfPoint, Result,
-};
+use opencv::{core, core::Vec3b, imgcodecs, imgproc, prelude::*, Result};
 
 fn detect_components() -> Result<()> {
-    let binary_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("binary.png");
+    let binary_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("greyscale.png");
     let components_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("components.png");
 
     let src = imgcodecs::imread(binary_path.to_str().unwrap(), imgcodecs::IMREAD_GRAYSCALE)?;
+    let mut src_bw = unsafe { Mat::new_size(src.size()?, core::CV_32S)? };
+    imgproc::threshold(&src, &mut src_bw, 128.0, 255.0, imgproc::THRESH_BINARY)?;
     let mut label_image = unsafe { Mat::new_size(src.size()?, core::CV_32S)? };
 
-    let num_labels = imgproc::connected_components(&src, &mut label_image, 8, core::CV_32S)?;
+    let num_labels = imgproc::connected_components(&src_bw, &mut label_image, 8, core::CV_32S)?;
     println!("num labels: {}", num_labels);
 
     let mut color_map: Vec<Vec3b> = vec![Vec3b::default(); num_labels as usize];
@@ -30,13 +30,14 @@ fn detect_components() -> Result<()> {
     let mut dest = unsafe { Mat::new_size(src.size()?, core::CV_8UC3)? };
     for row in 0..dest.rows()? {
         for col in 0..dest.cols()? {
-            let label = label_image.at_2d::<u8>(row, col)?;
+            let label = label_image.at_2d::<i32>(row, col)?;
+            // println!("{}", label);
             *dest.at_2d_mut::<Vec3b>(row, col)? = color_map[*label as usize];
         }
     }
 
-    // let params = opencv::types::VectorOfint::new();
-    // imgcodecs::imwrite(components_path.to_str().unwrap(), &labels, &params)?;
+    let params = opencv::types::VectorOfint::new();
+    imgcodecs::imwrite(components_path.to_str().unwrap(), &dest, &params)?;
 
     Ok(())
 }
