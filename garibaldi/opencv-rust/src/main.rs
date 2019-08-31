40313d9585f8edad46697b01d496faa8f17d2150
Mike Moran <mike@houseofmoran.com>
2019-09-01 00:11:46 +0100

    extract label_components

--- garibaldi/opencv-rust/src/main.rs
+++ garibaldi/opencv-rust/src/main.rs
@@ -19,6 +19,22 @@ fn random_color_map(num_labels: i32) -> Vec<Vec3b> {
     color_map
 }
 
+fn label_components(component_image: &Mat, num_components: i32) -> Result<Mat> {
+    println!("num labels: {}", num_components);
+
+    let color_map = random_color_map(num_components);
+
+    let mut label_image = unsafe { Mat::new_size(component_image.size()?, core::CV_8UC3)? };
+    for row in 0..label_image.rows()? {
+        for col in 0..label_image.cols()? {
+            let component = component_image.at_2d::<i32>(row, col)?;
+            *label_image.at_2d_mut::<Vec3b>(row, col)? = color_map[*component as usize];
+        }
+    }
+
+    Ok(label_image)
+}
+
 fn detect_components() -> Result<()> {
     let binary_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("greyscale.png");
     let components_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("components.png");
@@ -26,24 +42,14 @@ fn detect_components() -> Result<()> {
     let src = imgcodecs::imread(binary_path.to_str().unwrap(), imgcodecs::IMREAD_GRAYSCALE)?;
     let mut src_bw = unsafe { Mat::new_size(src.size()?, core::CV_32S)? };
     imgproc::threshold(&src, &mut src_bw, 128.0, 255.0, imgproc::THRESH_BINARY)?;
-    let mut label_image = unsafe { Mat::new_size(src.size()?, core::CV_32S)? };
+    let mut component_image = unsafe { Mat::new_size(src.size()?, core::CV_32S)? };
 
-    let num_labels = imgproc::connected_components(&src_bw, &mut label_image, 8, core::CV_32S)?;
-    println!("num labels: {}", num_labels);
-
-    let color_map = random_color_map(num_labels);
-
-    let mut dest = unsafe { Mat::new_size(src.size()?, core::CV_8UC3)? };
-    for row in 0..dest.rows()? {
-        for col in 0..dest.cols()? {
-            let label = label_image.at_2d::<i32>(row, col)?;
-            // println!("{}", label);
-            *dest.at_2d_mut::<Vec3b>(row, col)? = color_map[*label as usize];
-        }
-    }
+    let num_components =
+        imgproc::connected_components(&src_bw, &mut component_image, 8, core::CV_32S)?;
+    let label_image = label_components(&component_image, num_components)?;
 
     let params = opencv::types::VectorOfint::new();
-    imgcodecs::imwrite(components_path.to_str().unwrap(), &dest, &params)?;
+    imgcodecs::imwrite(components_path.to_str().unwrap(), &label_image, &params)?;
 
     Ok(())
 }
