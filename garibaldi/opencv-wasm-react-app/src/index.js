2e954197ae90731f30e116d72036d609ebc8c729
Mike Moran <mike@houseofmoran.com>
2019-09-02 00:10:13 +0100

    get wasm loading into react app
    used:
    - https://webpack.js.org/guides/code-splitting/
    - https://www.telerik.com/blogs/using-webassembly-with-react

--- garibaldi/opencv-wasm-react-app/src/index.js
+++ garibaldi/opencv-wasm-react-app/src/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
+
+function load_wasm() {
+  return import("@mike_moran/opencv-wasm-spike");
+}
+
+load_wasm().then(({ greet }) => {
+  greet("Farp");
+});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
