db03e713350f17d94378de57ad35a9814ad7bd49
Mike Moran <mike@houseofmoran.com>
2019-09-07 17:25:29 +0200

    remove unneeded binding

--- garibaldi/biscuiting-lib/src/lib.rs
+++ garibaldi/biscuiting-lib/src/lib.rs
@@ -3,11 +3,6 @@ extern crate wasm_bindgen;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern {
-    pub fn alert(s: &str);
-}
-
-#[wasm_bindgen]
 pub fn greet(name: &str) {
     use web_sys::console;
 
