c5c44d898b19340632b555ebb9bbf064fa71974d
Mike Moran <mike@houseofmoran.com>
2019-09-07 18:06:31 +0200

    decoded base64 bytes

--- garibaldi/biscuiting-lib/src/lib.rs
+++ garibaldi/biscuiting-lib/src/lib.rs
@@ -1,9 +1,11 @@
+extern crate base64;
 extern crate wasm_bindgen;
 
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn find_biscuits(image_data_uri_str: &str) {
+    use base64::decode;
     use url::Url;
     use web_sys::console;
 
@@ -26,6 +28,15 @@ pub fn find_biscuits(image_data_uri_str: &str) {
             if path.starts_with(prefix) {
                 let image_base64 = path.trim_start_matches(prefix);
                 console::log_1(&format!("base64: {}", image_base64).into());
+
+                match decode(image_base64) {
+                    Ok(image_bytes) => {
+                        console::log_1(&format!("got bytes: {}", image_bytes.len()).into());
+                    }
+                    Err(error) => {
+                        console::error_1(&format!("{:?}", error).into());
+                    }
+                }
             } else {
                 console::error_1(&format!("did not find {:?}", prefix).into());
             }
