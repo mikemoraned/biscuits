7b8eab47cd813e123b65c9d00c4e6f42ceac3417
Mike Moran <mike@houseofmoran.com>
2019-09-07 18:02:47 +0200

    get base64 of image data uri

--- garibaldi/biscuiting-lib/src/lib.rs
+++ garibaldi/biscuiting-lib/src/lib.rs
@@ -3,8 +3,35 @@ extern crate wasm_bindgen;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
+pub fn find_biscuits(image_data_uri_str: &str) {
+    use url::Url;
     use web_sys::console;
 
-    console::log_1(&format!("Hello, {}!", name).into());
+    console::log_1(&format!("received '{}'", image_data_uri_str).into());
+
+    match Url::parse(image_data_uri_str) {
+        Ok(image_data_uri) => {
+            console::log_1(&format!("uri: '{:?}'", image_data_uri).into());
+            console::log_1(
+                &format!(
+                    "uri parts: scheme: '{:?}', path: '{:?}'",
+                    image_data_uri.scheme(),
+                    image_data_uri.path()
+                )
+                .into(),
+            );
+
+            let path = image_data_uri.path();
+            let prefix = "image/png;base64,";
+            if path.starts_with(prefix) {
+                let image_base64 = path.trim_start_matches(prefix);
+                console::log_1(&format!("base64: {}", image_base64).into());
+            } else {
+                console::error_1(&format!("did not find {:?}", prefix).into());
+            }
+        }
+        Err(error) => {
+            console::error_1(&format!("{:?}", error).into());
+        }
+    }
 }
