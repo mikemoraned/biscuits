26f5a6677e9791175c19b40fb5b5607c0a243df0
Mike Moran <mike@houseofmoran.com>
2019-09-07 16:03:27 +0200

    minimal version which loads a wasm module locally
    followed
    - https://create-react-app.dev/docs/code-splitting for
    pattern of loading
    - https://www.telerik.com/blogs/using-webassembly-with-react for using specific wasm loader
    - https://medium.com/@the1mills/how-to-test-your-npm-module-without-publishing-it-every-5-minutes-1c4cb4b369be , which led to:
      - `npm link`, in separate rust repo
      - `npm link @mike_moran/biscuiting-lib` here, to link it in

--- garibaldi/wasm-biscuits-frontend/src/App.js
+++ garibaldi/wasm-biscuits-frontend/src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  handleClick = () => {
+    import("@mike_moran/biscuiting-lib")
+      .then(biscuiting => {
+        console.dir(biscuiting);
+        biscuiting.greet("foop");
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>Load</button>
+      </div>
+    );
+  }
 }
 
 export default App;
