24b8104db4930ac743f8fad1746cb7080c612c9f
Mike Moran <mike@houseofmoran.com>
2019-09-08 21:43:59 +0200

    got it working using ClampedArrays to communicate
    this is a lot faster: 220ms as opposed to around 1000ms

--- garibaldi/biscuiting-lib/src/lib.rs
+++ garibaldi/biscuiting-lib/src/lib.rs
@@ -3,21 +3,59 @@ extern crate image;
 extern crate wasm_bindgen;
 
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::Clamped;
 
 #[wasm_bindgen]
 pub struct BiscuitFinder {
-    width: u8,
-    height: u8,
+    width: u32, 
+    height: u32,
+    output: Option<Vec<u8>>,
 }
 
 #[wasm_bindgen]
 impl BiscuitFinder {
-    pub fn new(width: u8, height: u8) -> BiscuitFinder {
-        BiscuitFinder { width, height }
+
+    pub fn new(width: u32, height: u32) -> BiscuitFinder {
+        BiscuitFinder {
+            width,
+            height,
+            output: None
+        }
     }
 
-    pub fn foop(&self) -> String {
-        "foop!".into()
+    pub fn find_biscuits(&mut self, input: Clamped<Vec<u8>>) -> Result<String, JsValue> {
+        use image::{ RgbaImage};
+        use imageproc::noise::salt_and_pepper_noise;
+        use web_sys::console;
+
+        console::time_with_label("from raw input");
+        match RgbaImage::from_raw(self.width, self.height, input.0) {
+            Some(image) => {
+                console::time_end_with_label("from raw input");
+
+                console::time_with_label("process image");
+                let processed_image = salt_and_pepper_noise(&image, 0.1, 1);
+                console::time_end_with_label("process image");
+
+                console::time_with_label("to raw output");
+                self.output = Some(processed_image.to_vec());
+                console::time_end_with_label("to raw output");
+
+                return Ok("processed image".into());
+            },
+            None => {
+                return Err("couldn't read from raw".into());
+            }
+        }
+    }
+
+    pub fn output(&self) -> *const u8 {
+        match &self.output {
+            Some(buffer) => {
+                buffer.as_ptr()
+            }
+            None => panic!("no output")
+        }
     }
 }
 
