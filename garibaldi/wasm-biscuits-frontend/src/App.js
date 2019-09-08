594f0ab00ced2d3c53eee020b7d6ebb653b525d9
Mike Moran <mike@houseofmoran.com>
2019-09-08 21:43:59 +0200

    got it working using ClampedArrays to communicate
    this is a lot faster: 220ms as opposed to around 1000ms

--- garibaldi/wasm-biscuits-frontend/src/App.js
+++ garibaldi/wasm-biscuits-frontend/src/App.js
@@ -4,28 +4,51 @@ function App() {
   const canvasRef = useRef(null);
 
   const handleClick = () => {
-    import("@mike_moran/biscuiting-lib")
-      .then(biscuiting => {
+    Promise.all([
+      import("@mike_moran/biscuiting-lib"),
+      import("@mike_moran/biscuiting-lib/biscuiting_lib_bg")
+    ])
+      .then(([biscuiting, biscuiting_bg]) => {
         const canvas = canvasRef.current;
-        const { BiscuitFinder } = biscuiting;
-        const biscuitFinder = BiscuitFinder.new(canvas.width, canvas.height);
-        console.dir(biscuitFinder);
-        console.dir(biscuitFinder.foop());
-
-        const dataURL = canvas.toDataURL();
-        console.time("find biscuits");
-        const result = biscuiting.find_biscuits(dataURL);
-        console.timeEnd("find biscuits");
         const context = canvas.getContext("2d");
-        const image = new Image();
-        image.onload = () => {
-          console.timeEnd("load image");
-          console.time("draw image");
-          context.drawImage(image, 0, 0);
-          console.timeEnd("draw image");
-        };
-        console.time("load image");
-        image.src = result;
+        const { width, height } = canvas;
+        const { BiscuitFinder } = biscuiting;
+        const { memory } = biscuiting_bg;
+        const biscuitFinder = BiscuitFinder.new(width, height);
+
+        const inputImageData = context.getImageData(0, 0, width, height);
+        console.time("find biscuits");
+        console.dir(biscuitFinder.find_biscuits(inputImageData.data));
+        console.timeEnd("find biscuits");
+
+        console.time("draw image");
+        const outputPointer = biscuitFinder.output();
+        console.dir(outputPointer);
+        const outputArray = new Uint8ClampedArray(
+          memory.buffer,
+          outputPointer,
+          4 * width * height
+        );
+
+        const outputImageData = new ImageData(outputArray, width, height);
+
+        context.putImageData(outputImageData, 0, 0);
+        console.timeEnd("draw image");
+
+        // const dataURL = canvas.toDataURL();
+        // console.time("find biscuits");
+        // const result = biscuiting.find_biscuits(dataURL);
+        // console.timeEnd("find biscuits");
+
+        // const image = new Image();
+        // image.onload = () => {
+        //   console.timeEnd("load image");
+        //   console.time("draw image");
+        //   context.drawImage(image, 0, 0);
+        //   console.timeEnd("draw image");
+        // };
+        // console.time("load image");
+        // image.src = result;
       })
       .catch(err => {
         console.log(err);
