c38f6cce4957c52e905e775b6c705776f6344179
Mike Moran <mike@houseofmoran.com>
2019-09-16 22:32:22 +0100

    remove unneeded debugging + fix to be multicolor

--- garibaldi/biscuiting-lib/src/lib.rs
+++ garibaldi/biscuiting-lib/src/lib.rs
@@ -38,7 +38,12 @@ impl BiscuitFinder {
         let mut color_map = vec![Rgba([0u8; 4]); num_labels];
         color_map[0] = Rgba([0u8; 4]);
         for label in 1..num_labels {
-            color_map[label] = Rgba([gen_range(1, 255), gen_range(1, 255), gen_range(1, 255), 0u8]);
+            color_map[label] = Rgba([
+                gen_range(1, 255),
+                gen_range(1, 255),
+                gen_range(1, 255),
+                255u8,
+            ]);
         }
         console::log_1(&format!("color map: {:?}", color_map).into());
 
@@ -51,43 +56,10 @@ impl BiscuitFinder {
         use imageproc::region_labelling::{connected_components, Connectivity};
         use web_sys::console;
 
-        let len = input.0.len();
-        console::log_1(
-            &format!(
-                "raw input [{}, {}, {}, {}], [{}, {}, {}, {}]",
-                input.0[0],
-                input.0[1],
-                input.0[2],
-                input.0[3],
-                input.0[len - 4],
-                input.0[len - 3],
-                input.0[len - 2],
-                input.0[len - 1]
-            )
-            .into(),
-        );
-
         console::time_with_label("from raw input");
         match RgbaImage::from_raw(self.width, self.height, input.0) {
             Some(image) => {
                 console::time_end_with_label("from raw input");
-
-                let top_left = image.get_pixel(0, 0);
-                let bottom_right = image.get_pixel(self.width - 1, self.height - 1);
-                console::log_1(
-                    &format!(
-                        "rgba [{}, {}, {}, {}], [{}, {}, {}, {}]",
-                        top_left[0],
-                        top_left[1],
-                        top_left[2],
-                        top_left[3],
-                        bottom_right[0],
-                        bottom_right[1],
-                        bottom_right[2],
-                        bottom_right[3]
-                    )
-                    .into(),
-                );
 
                 console::time_with_label("process image");
                 let gray_image: GrayImage = map_colors(&image, |p| {
@@ -113,14 +85,8 @@ impl BiscuitFinder {
                 console::time_end_with_label("process image");
 
                 console::time_with_label("to raw output");
-                // let remapped_to_rgba_image: RgbaImage = map_colors(&gray_image, |p| {
-                let remapped_to_rgba_image: RgbaImage = map_colors(&processed_gray_image, |p| {
-                    let gray = p[0];
-                    Rgba([gray, gray, gray, 255])
-                });
-                self.output = Some(remapped_to_rgba_image.to_vec());
+                self.output = Some(processed_gray_image.to_vec());
                 console::time_end_with_label("to raw output");
-
                 return Ok("processed image".into());
             }
             None => {
