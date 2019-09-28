5b02d6910b5c183a1b6f2c0c677de9846823d187
Mike Moran <mike@houseofmoran.com>
2019-09-28 16:50:13 +0100

    showing random circles
    not sure why there is a line in from middle of circle
    to edge; does not matter

--- garibaldi/canvas-resolution-learn/src/App.js
+++ garibaldi/canvas-resolution-learn/src/App.js
@@ -1,24 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useEffect, useState, useReducer } from "react";
+import React from "react";
+import "./App.css";
+import { stat } from "fs";
 
 function App() {
+  const requiredPoints = 100;
+  const canvasRef = useRef();
+  const [state, dispatch] = useReducer(
+    (state, action) => {
+      switch (action.type) {
+        case "dimensionsKnown":
+          const { width, height } = action.dimensions;
+          var points = [];
+          [...Array(requiredPoints)].forEach(() => {
+            points.push({
+              x: Math.random() * width,
+              y: Math.random() * height,
+              radius: 10
+            });
+          });
+          return {
+            points
+          };
+        default:
+          throw new Error();
+      }
+    },
+    { points: [] }
+  );
+
+  useEffect(() => {
+    console.log("Hola");
+
+    if (canvasRef.current) {
+      const canvas = canvasRef.current;
+      const { width, height } = canvas;
+
+      dispatch({ type: "dimensionsKnown", dimensions: { width, height } });
+    }
+  }, [canvasRef]);
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      const canvas = canvasRef.current;
+      const context = canvas.getContext("2d");
+      context.strokeStyle = "black";
+      context.beginPath();
+      state.points.forEach(({ x, y, radius }) => {
+        context.moveTo(x, y);
+        context.arc(x, y, radius, 0, Math.PI * 2);
+      });
+      context.stroke();
+    }
+  }, [canvasRef, state]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <canvas
+          ref={canvasRef}
+          width="400"
+          height="400"
+          style={{ border: "1px dashed black" }}
+        ></canvas>
+      </div>
+      <input type="range"></input>
     </div>
   );
 }
