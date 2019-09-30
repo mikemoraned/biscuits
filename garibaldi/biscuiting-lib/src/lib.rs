f316791c728039c58d0960e669c3a5bf250b6296
Mike Moran <mike@houseofmoran.com>
2019-09-30 23:20:26 +0100

    first compile, based on example. does nothing
    based on example https://github.com/rustsim/nphysics/blob/master/examples2d/boxes2.rs

--- garibaldi/biscuiting-lib/src/lib.rs
+++ garibaldi/biscuiting-lib/src/lib.rs
@@ -13,6 +13,82 @@ use wasm_bindgen::Clamped;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+extern crate nalgebra as na;
+use na::{Point2, Vector2};
+use ncollide2d::shape::{Cuboid, ShapeHandle};
+use nphysics2d::force_generator::DefaultForceGeneratorSet;
+use nphysics2d::joint::DefaultJointConstraintSet;
+use nphysics2d::object::{
+    BodyPartHandle, ColliderDesc, DefaultBodySet, DefaultColliderSet, Ground, RigidBodyDesc,
+};
+use nphysics2d::world::{DefaultGeometricalWorld, DefaultMechanicalWorld};
+
+#[wasm_bindgen]
+pub struct BallPhysicsSimulation {}
+
+pub struct Ball {}
+
+#[wasm_bindgen]
+impl BallPhysicsSimulation {
+    pub fn new() -> BallPhysicsSimulation {
+        /*
+         * World
+         */
+        let mut mechanical_world = DefaultMechanicalWorld::new(Vector2::new(0.0, -9.81));
+        let mut geometrical_world = DefaultGeometricalWorld::new();
+        let mut bodies = DefaultBodySet::new();
+        let mut colliders = DefaultColliderSet::new();
+        let joint_constraints = DefaultJointConstraintSet::<f32>::new();
+        let force_generators = DefaultForceGeneratorSet::<f32>::new();
+
+        /*
+         * Ground
+         */
+        let ground_size = 25.0;
+        let ground_shape = ShapeHandle::new(Cuboid::new(Vector2::new(ground_size, 1.0)));
+
+        let ground_handle = bodies.insert(Ground::new());
+        let co = ColliderDesc::new(ground_shape)
+            .translation(-Vector2::y())
+            .build(BodyPartHandle(ground_handle, 0));
+        colliders.insert(co);
+
+        /*
+         * Create the boxes
+         */
+        let num = 10;
+        let rad = 0.1;
+
+        let cuboid = ShapeHandle::new(Cuboid::new(Vector2::repeat(rad)));
+
+        let shift = (rad + ColliderDesc::<f32>::default_margin()) * 2.0;
+        let centerx = shift * (num as f32) / 2.0;
+        let centery = shift / 2.0;
+
+        for i in 0usize..num {
+            for j in 0..num {
+                let x = i as f32 * shift - centerx;
+                let y = j as f32 * shift + centery;
+
+                // Build the rigid body.
+                let rb = RigidBodyDesc::new().translation(Vector2::new(x, y)).build();
+                let rb_handle = bodies.insert(rb);
+
+                // Build the collider.
+                let co = ColliderDesc::new(cuboid.clone())
+                    .density(1.0)
+                    .build(BodyPartHandle(rb_handle, 0));
+                colliders.insert(co);
+            }
+        }
+
+        mechanical_world.counters.enable();
+        geometrical_world.maintain(&mut bodies, &mut colliders);
+
+        BallPhysicsSimulation {}
+    }
+}
+
 #[wasm_bindgen]
 pub struct BiscuitFinder {
     width: u32,
