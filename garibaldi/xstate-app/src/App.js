0f03b6b609c43cc5ad24554e6fbb8c60b0a059ca
Mike Moran <mike@houseofmoran.com>
2019-10-13 12:44:39 +0100

    basic toggle xstate example

--- garibaldi/xstate-app/src/App.js
+++ garibaldi/xstate-app/src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Machine, interpret } from "xstate";
+
+const toggleMachine = Machine({
+  id: "toggle",
+  initial: "inactive",
+  states: {
+    inactive: { on: { TOGGLE: "active" } },
+    active: { on: { TOGGLE: "inactive" } }
+  }
+});
+
+const toggleService = interpret(toggleMachine)
+  .onTransition(state => console.log(state.value))
+  .start();
 
 function App() {
+  function toggle() {
+    toggleService.send("TOGGLE");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={toggle}>toggle</button>
     </div>
   );
 }
