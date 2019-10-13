4fe7cec4e68d70a67f4f982b0c5fa57477e3c6ff
Mike Moran <mike@houseofmoran.com>
2019-10-13 21:44:14 +0100

    trying invoke
    based on https://xstate.js.org/docs/patterns/sequence.html#async-sequences

--- garibaldi/xstate-app/src/App.js
+++ garibaldi/xstate-app/src/App.js
@@ -3,27 +3,84 @@ import "./App.css";
 import { Machine } from "xstate";
 import { useMachine } from "@xstate/react";
 
-const toggleMachine = Machine({
-  id: "toggle",
-  initial: "inactive",
+const fetchMap = () => {
+  console.log("fetching map...");
+  return new Promise((resolve, reject) => {
+    setTimeout(function() {
+      const random = Math.random();
+      console.log(random);
+      if (random > 0.5) {
+        resolve();
+      } else {
+        reject();
+      }
+    }, 1000);
+  });
+};
+
+const mapMachine = Machine({
+  id: "map",
+  initial: "loading",
   states: {
-    inactive: { on: { TOGGLE: "active" } },
-    active: { on: { TOGGLE: "inactive" } }
+    loading: {
+      invoke: {
+        id: "fetchMap",
+        src: (context, event) => fetchMap(),
+        onDone: {
+          target: "interactive"
+        },
+        onError: {
+          target: "loading_failed"
+        }
+      }
+    },
+    loading_failed: {
+      on: {
+        RETRY: "loading"
+      }
+    },
+    interactive: {
+      on: {
+        RETRY: "loading"
+      }
+    }
   }
 });
 
-function App() {
-  const [current, send] = useMachine(toggleMachine);
+function Reload() {
+  const [current, send] = useMachine(mapMachine);
 
-  function toggle() {
-    send("TOGGLE");
+  return <button onClick={() => send("RETRY")}>Retry</button>;
+}
+
+function Map() {
+  const [current, send] = useMachine(mapMachine);
+
+  if (current.matches("loading")) {
+    return <div>Loading</div>;
+  } else if (current.matches("loading_failed")) {
+    return (
+      <div>
+        <div>Loading failed</div>
+        <Reload />
+      </div>
+    );
+  } else if (current.matches("interactive")) {
+    return (
+      <div>
+        <div>Interactive</div>
+        <Reload />
+      </div>
+    );
+  } else {
+    return <div></div>;
   }
+}
 
+function App() {
   return (
     <div className="App">
-      <button onClick={toggle}>
-        {current.matches("inactive") ? "Off" : "On"}
-      </button>
+      <Map />
     </div>
   );
 }
