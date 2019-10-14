55ba1a6e7d983611dc00b758176fcf27ff050ba4
Mike Moran <mike@houseofmoran.com>
2019-10-14 22:36:47 +0100

    extract MapMachine

--- garibaldi/xstate-app/src/App.js
+++ garibaldi/xstate-app/src/App.js
@@ -2,56 +2,8 @@
 
 import React from "react";
 import "./App.css";
-import { Machine, interpret } from "xstate";
+import { mapService } from "./MapMachine";
 import { useService } from "@xstate/react";
-
-const fetchMap = () => {
-  console.log("fetching map...");
-  return new Promise((resolve, reject) => {
-    setTimeout(function() {
-      const random = Math.random();
-      console.log(random);
-      if (random > 0.5) {
-        resolve();
-      } else {
-        reject();
-      }
-    }, 1000);
-  });
-};
-
-const mapMachine = Machine({
-  id: "map",
-  initial: "loading",
-  states: {
-    loading: {
-      invoke: {
-        id: "fetchMap",
-        src: fetchMap,
-        onDone: {
-          target: "interactive"
-        },
-        onError: {
-          target: "loading_failed"
-        }
-      }
-    },
-    loading_failed: {
-      on: {
-        RETRY: "loading"
-      }
-    },
-    interactive: {
-      on: {
-        RETRY: "loading"
-      }
-    }
-  }
-});
-
-const mapService = interpret(mapMachine)
-  .onTransition(state => console.log(state.value))
-  .start();
 
 function Reload() {
   const [_, send] = useService(mapService);
