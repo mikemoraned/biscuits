3d269383273e0193bd89ddf7695f2971ab0f9b93
Mike Moran <mike@houseofmoran.com>
2019-10-19 15:48:43 +0100

    add explicit subreddit machine

--- garibaldi/xstate-reddit-app/src/App.test.js
+++ garibaldi/xstate-reddit-app/src/App.test.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { interpret } from "xstate";
 
-import { redditMachine } from "./Machine";
+import { createSubredditMachine } from "./Machine";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
@@ -12,20 +12,14 @@ it("renders without crashing", () => {
 });
 
 it("should load posts of a selected subreddit", done => {
-  const redditService = interpret(redditMachine)
-    .onTransition(state => {
-      // when the state finally reaches 'selected.loaded',
-      // the test has succeeded.
+  const machine = createSubredditMachine("reactjs");
+  const redditService = interpret(machine).onTransition(state => {
+    if (state.matches("loaded")) {
+      expect(state.context.posts.length).toBeGreaterThan(0);
 
-      if (state.matches({ selected: "loaded" })) {
-        expect(state.context.posts.length).toBeGreaterThan(0);
+      done();
+    }
+  });
 
-        done();
-      }
-    })
-    .start(); // remember to start the service!
-
-  // Test that when the 'SELECT' event is sent, the machine eventually
-  // reaches the { selected: 'loaded' } state with posts
-  redditService.send("SELECT", { name: "reactjs" });
+  redditService.start();
 });
