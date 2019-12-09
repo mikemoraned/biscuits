218463568fd88e7e37c690f22ff5565d5e8454b0
Mike Moran <mike@houseofmoran.com>
2019-12-09 18:07:59 +0000

    add some more example cities

--- garibaldi/spikes/redraw-layer/src/App.js
+++ garibaldi/spikes/redraw-layer/src/App.js
@@ -17,14 +17,20 @@ const cities = [
     location: { latitude: 31.768318, longitude: 35.213711 }
   },
   { name: "Berlin", location: { latitude: 52.520008, longitude: 13.404954 } },
-  { name: "London", location: { latitude: 51.507351, longitude: -0.127758 } }
+  { name: "London", location: { latitude: 51.507351, longitude: -0.127758 } },
+  {
+    name: "Edinburgh",
+    location: { latitude: 55.953251, longitude: -3.188267 }
+  },
+  { name: "New York", location: { latitude: 40.73061, longitude: -73.935242 } },
+  { name: "Budapest", location: { latitude: 47.497913, longitude: 19.040236 } }
 ];
 
 function App() {
   return (
     <MapBoxContextProviderFromEnvironment>
       <div>
-        <MapView city={cities[0]} />
+        <MapView city={cities[6]} />
       </div>
     </MapBoxContextProviderFromEnvironment>
   );
