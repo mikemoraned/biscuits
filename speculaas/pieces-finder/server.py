bde991ee1a54d4931bebf0d09fb34afff06d367e
Mike Moran <mike@houseofmoran.com>
2019-12-28 19:51:54 +0100

    optionally set up Honeycomb tracing

--- speculaas/pieces-finder/server.py
+++ speculaas/pieces-finder/server.py
@@ -8,41 +8,55 @@ from precomputed_lookup_splitter import PreComputedLookupSplitter
 from schema import schema
 from flask_cors import CORS
 
+from dotenv import load_dotenv
+import os
+import beeline
+from beeline.middleware.flask import HoneyMiddleware
+
 from splitter_cache import SplitterCache
 
-app = Flask(__name__)
-CORS(app)
+load_dotenv()
 
 logging.basicConfig(level=logging.DEBUG)
 
+app = Flask(__name__)
+CORS(app)
+honeycomb_key = os.getenv("HONEYCOMB_KEY")
+if honeycomb_key:
+    logging.info("honeycomb: initializing")
+    beeline.init(
+        writekey=honeycomb_key, dataset="speculaas", service_name="pieces-finder"
+    )
+    HoneyMiddleware(app, db_events=False)
+    logging.info("honeycomb: initialized")
+else:
+    logging.info("honeycomb: skipping")
+
 precomputed_dir = sys.argv[1]
 layout_dir = sys.argv[2]
-layout_registry=LayoutRegistry.from_dir(layout_dir)
+layout_registry = LayoutRegistry.from_dir(layout_dir)
 splitter = PreComputedLookupSplitter.from_dir(precomputed_dir, layout_registry)
 splitter_cache = SplitterCache(splitter)
 pre_cache_completed = False
 
 
-@app.route('/healthcheck/alive')
+@app.route("/healthcheck/alive")
 def alive():
     return "Alive"
 
 
-@app.route('/healthcheck/ready')
+@app.route("/healthcheck/ready")
 def ready():
     splitter_cache.pre_cache_all_place_ids()
     return "Ready"
 
 
 app.add_url_rule(
-    '/graphql',
+    "/graphql",
     view_func=GraphQLView.as_view(
-        'graphql',
-        schema=schema,
-        root_value=splitter,
-        graphiql=True
-    )
+        "graphql", schema=schema, root_value=splitter, graphiql=True
+    ),
 )
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
