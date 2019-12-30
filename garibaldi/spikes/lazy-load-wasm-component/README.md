cc5bc339d271bb56de7312d19fe7d2532d3a2916
Mike Moran <mike@houseofmoran.com>
2019-12-30 15:09:50 +0100

    Convert the thing which needs wasm library into a callback function
    - this means we lose the need for an extra level of
      dynamic imports

--- garibaldi/spikes/lazy-load-wasm-component/README.md
+++ garibaldi/spikes/lazy-load-wasm-component/README.md
@@ -4,3 +4,4 @@
 - [x] Wrap in loader function and add artificial delay
 - [x] return a dynamic definition of a module, which binds to local values
 - [x] Convert vanilla component to instead lazy load biscuiting lib
+- [x] Convert the thing which needs wasm library into a callback function
--- garibaldi/spikes/lazy-load-wasm-component/src/App.js
+++ garibaldi/spikes/lazy-load-wasm-component/src/App.js
@@ -1,31 +1,9 @@
 import React from "react";
 import { Suspense } from "react";
 import "./App.css";
+import { lazyLoader } from "./BiscuitingComponent";
 
-function loaderFunction() {
-  console.time("biscuiting");
-  return Promise.all([
-    import("@mike_moran/biscuiting-lib"),
-    import("@mike_moran/biscuiting-lib/biscuiting_lib_bg")
-  ]).then(([biscuiting_lib, biscuiting_lib_bg]) => {
-    console.timeEnd("biscuiting");
-    console.time("import");
-    return import("./BiscuitingComponent").then(
-      ({ bindBiscuitingComponent }) => {
-        console.timeEnd("import");
-        console.time("binding");
-        const BiscuitingComponent = bindBiscuitingComponent({
-          biscuiting_lib,
-          biscuiting_lib_bg
-        });
-        console.timeEnd("binding");
-        return { default: BiscuitingComponent };
-      }
-    );
-  });
-}
-
-const BiscuitingComponent = React.lazy(loaderFunction);
+const BiscuitingComponent = React.lazy(lazyLoader);
 
 function App() {
   return (
