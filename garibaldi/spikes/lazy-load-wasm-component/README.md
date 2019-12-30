4760a6dde7a7c9406b955caed32f8e0a7f4e1af4
Mike Moran <mike@houseofmoran.com>
2019-12-30 13:47:20 +0100

    basic vanilla component + define Spike
    - `npx create-react-app lazy-load-wasm-component`
    - reduce to only basics + add VanillaComponent
    - define purpose of spike in README

---
+++ garibaldi/spikes/lazy-load-wasm-component/README.md
@@ -0,0 +1,4 @@
+# Spike: Lazy Loading Wasm Component
+
+- [ ] Use React Suspense / Lazy to lazy-load a vanilla component
+- [ ] Convert vanilla component to instead lazy load biscuiting lib
---
+++ garibaldi/spikes/lazy-load-wasm-component/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import "./VanillaComponent";
+import { VanillaComponent } from "./VanillaComponent";
+
+function App() {
+  return (
+    <div>
+      <VanillaComponent />
+    </div>
+  );
+}
+
+export default App;
